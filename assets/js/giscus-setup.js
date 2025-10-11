34373baadebb09f9148fae1cef1a6ab4c5a0c59f
dcamacho16 <camacho.daniel11@gmail.com>
2025-10-11 19:19:30 +0000

    Deploying to gh-pages from @ dcamacho16/dcamacho16.github.io@8f1e1064771b996ec768edb2fe66307a5fcf21f9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "dcamacho16/dcamacho16",
+    "data-repo": "dcamacho16/dcamacho16.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
